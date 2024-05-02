d9df84cf22586c872012c325ff7722966b8c8d9b
ksr03 <K_Haruka03@outlook.jp>
2024-05-03 01:28:22 +0900

    背景を消して、光を調節した

--- src/components/3D/Viewport3D.tsx
+++ src/components/3D/Viewport3D.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { OrbitControls, Environment } from '@react-three/drei'
+import { OrbitControls, Environment, useEnvironment } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import LoadModels from './LoadModels'
 import type { furnitureType } from '../../type/furnitureType'
@@ -14,14 +14,16 @@ function Viewport3D ({ furnitureList }: Props): JSX.Element {
       <LoadModels key={index} url={`${fileName}.gltf`} position={position} rotation={[0, -(rotation * 0.01745329), 0]}/>
     )
   })
+  const envMap = useEnvironment({ files: '/HDR/kloppenheim_07_puresky_1k.hdr' })
   return (
     <Canvas style={{ width: '700px', height: '700px' }}>
+      <ambientLight />
       <OrbitControls />
       {ModelList}
       <LoadModels url={'floor.gltf'} position={[0, -0.5, 0]} rotation={[0, 0, 0]}/>
       { /* eslint-disable-next-line */ }
       <pointLight position={[10, 10, 10]} />
-      <Environment preset='sunset' blur={0.7} background />
+      <Environment map={envMap} blur={1.0} />
     </Canvas>
   )
 }
